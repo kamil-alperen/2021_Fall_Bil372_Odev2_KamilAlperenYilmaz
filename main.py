82cef6774051fa0da18069b5e0aaf38353d83768
kamil-alperen <63701451+kamil-alperen@users.noreply.github.com>
2021-11-15 22:41:03 +0300

    prdd

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.hybrid import hybrid_property
 key = Fernet.generate_key()
 fernet = Fernet(key) 
 app = Flask(__name__)
-ENV = 'dev'
+ENV = 'prod'
 
 if(ENV == 'dev'):
     app.debug = True
